66c77ff4724b2379fc3da5cdca96a4d624c14573
GabrielFilomeno <biel_floripa@hotmail.com>
2024-07-23 20:41:59 -0300

    commit-inicial

---
+++ src/main/java/com/example/LABMedical_API/LabMedicalApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.LABMedical_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabMedicalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabMedicalApiApplication.class, args);
+	}
+
+}
