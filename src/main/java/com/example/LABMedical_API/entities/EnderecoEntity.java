8760bc0024e3aced5501d6e2580e39de2d92a774
GabrielFilomeno <biel_floripa@hotmail.com>
2024-07-24 19:53:08 -0300

    feature-atributos-paciente

--- src/main/java/com/example/LABMedical_API/entities/EnderecoEntity.java
+++ src/main/java/com/example/LABMedical_API/entities/EnderecoEntity.java
@@ -1,9 +1,35 @@
 package com.example.LABMedical_API.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "enderecos")
 public class EnderecoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long enderecoId;
+
+    @NotNull
+    private Integer cep;
+
+    @NotBlank
+    private String cidade;
+
+    @NotBlank
+    private String estado;
+
+    @NotBlank
+    private String logradouro;
+
+    private Integer numero;
+
+    private String complemento;
+
+    @NotBlank
+    private String bairro;
+
+    private String pontoReferencia;
 }
