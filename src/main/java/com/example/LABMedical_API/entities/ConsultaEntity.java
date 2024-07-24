f899c7fd0be7e8da8dcf30f4ed06915c93115e7f
GabrielFilomeno <biel_floripa@hotmail.com>
2024-07-23 21:11:06 -0300

    feature-entidades-criadas

---
+++ src/main/java/com/example/LABMedical_API/entities/ConsultaEntity.java
@@ -0,0 +1,9 @@
+package com.example.LABMedical_API.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "consultas")
+public class ConsultaEntity {
+}
---
+++ src/main/java/com/example/LABMedical_API/entities/UsuarioEntity.java
@@ -0,0 +1,9 @@
+package com.example.LABMedical_API.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class UsuarioEntity {
+}
